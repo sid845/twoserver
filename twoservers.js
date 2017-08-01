97a81ac9d96bb8a1150b242db4e6f8e4cd37daf8
Siddharth Patel <siddharthpatel845@gmail.com>
2017-08-01 19:27:18 -0400

    fdgv

--- twoservers.js
+++ twoservers.js
@@ -1,7 +1,7 @@
 var http=require('http');
 
 var PORT = 7000;
-var PORT = 7500;
+var PORT2 = 7500;
 
 function handleRequest(req, res){
   res.end('kfbnlnkgdblfv'+req.url);
@@ -14,6 +14,6 @@ var server2=http.createServer(handleRequest2);
 server.listen(PORT, function(){
   console.log("server listening on: http://localhost:"+PORT);
 });
-server2.listen(PORT, function(){
-  console.log("server listening on: http://localhost:"+PORT);
+server2.listen(PORT2, function(){
+  console.log("server listening on: http://localhost:"+PORT2);
 });
