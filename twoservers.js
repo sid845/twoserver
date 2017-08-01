052a5c28ff12d230921cd4264e93f3dcb899eefc
Siddharth Patel <siddharthpatel845@gmail.com>
2017-08-01 19:24:10 -0400

    fdgv

---
+++ twoservers.js
@@ -0,0 +1,16 @@
+var http=require('http');
+
+var PORT = 7000;
+var PORT = 7500;
+
+function handleRequest(req, res){
+  res.end('kfbnlnkgdblfv'+req.url);
+}
+function handleRequest2(req,res){
+  res.end('fkkknbfldkvnfodgb'+req.url);
+}
+var server=http.createServer(handleRequest);
+var server2=http.createServer(handleRequest2);
+server.listen(PORT, function(){
+  console.log("server listening on: http://localhost:"+PORT);
+});
